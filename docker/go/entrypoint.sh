9f7f4cfc6a2a591b20fbb1e5c73f253dc648ccfa
yoshimura <karateyosi@gmail.com>
2022-10-23 20:33:50 +0900

    Clean

--- docker/go/entrypoint.sh
+++ docker/go/entrypoint.sh
@@ -12,11 +12,17 @@ until nc -z db 3306; do
 done
 >&1 echo "db container is now fully activated. - [Running]"
 
+# docker-composee up
+# が実行されたとき
 if [ $_ENV = "dev" ];then
   >&1 echo "> go run app/cmd/main.go"
   air -c .air.toml
+# docker-compose -f docker-compose.yml -f production.yml up
+# が実行されたとき
 elif [ $_ENV = "prod" ];then
   >&1 echo "> go build app/cmd/main.go"
   go run app/cmd/main.go
   ./main
+else
+  >&2 echo "Missing _ENV: 環境変数 '_ENV' が設定されていません. - [Failed]"
 fi
